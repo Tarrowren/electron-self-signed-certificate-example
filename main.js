1ae5ed4482e2afc1fb20a275f36a3ee744498d83
Tarrowren <tarrowren@foxmail.com>
2022-08-06 13:14:24 +0800

    refactor

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
+const { BufferReadable } = require("buffer-readable");
 const { app, BrowserWindow, protocol } = require("electron");
 const { createReadStream } = require("fs");
 const { readFile } = require("fs/promises");
-const { BufferReadable } = require("buffer-readable");
+const { Agent, request: _request } = require("https");
 const { MultiBufferReadable } = require("multi-readable");
 const { join } = require("path");
-const https = require("https");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -15,55 +15,52 @@ function createWindow() {
     },
   });
 
-  win.loadURL("tarrow://./");
+  win.loadURL("https://127.0.0.1:8080");
+  // win.loadURL("https://developer.mozilla.org");
 }
 
-protocol.registerSchemesAsPrivileged([
-  {
-    scheme: "tarrow",
-    privileges: {
-      standard: true,
-      secure: true,
-      bypassCSP: true,
-      allowServiceWorkers: true,
-      supportFetchAPI: true,
-      corsEnabled: true,
-      stream: true,
-    },
-  },
-]);
-
 app.whenReady().then(async () => {
-  const agent = new https.Agent({
-    ca: await readFile("cert/ca.crt"),
-    cert: await readFile("cert/client.crt"),
-    key: await readFile("cert/client.key"),
-  });
+  const [ca, cert, key] = await Promise.all([
+    readFile("cert/ca.crt"),
+    readFile("cert/client.crt"),
+    readFile("cert/client.key"),
+  ]);
+  const agent = new Agent({ ca, cert, key });
 
-  async function _request(options, stream) {
-    return await new Promise((resolve, reject) => {
-      options.agent = agent;
+  protocol.interceptStreamProtocol("https", (request, callback) => {
+    const url = new URL(request.url);
 
-      const req = https
-        .request(options, resolve)
-        .on("error", reject)
-        .on("timeout", () => {
-          req.destroy(new Error("timeout"));
+    const req = _request(
+      {
+        hostname: url.hostname,
+        port: url.port,
+        path: url.pathname + url.search + url.hash,
+        method: request.method,
+        headers: request.headers,
+        agent: url.hostname === "127.0.0.1" ? agent : false,
+        timeout: 10000,
+      },
+      (res) => {
+        callback({
+          data: res,
+          headers: res.headers,
+          method: request.method,
+          referrer: request.referrer,
+          statusCode: res.statusCode,
         });
-
-      if (stream) {
-        stream.pipe(req);
-      } else {
-        req.end();
       }
-    });
-  }
+    )
+      .on("error", (err) => {
+        throw err;
+      })
+      .on("timeout", () => {
+        req.destroy(new Error("timeout"));
+      });
 
-  protocol.registerStreamProtocol("tarrow", async (request, callback) => {
-    let stream;
-    if (request.uploadData) {
-      stream = new MultiBufferReadable(
-        request.uploadData.map(({ bytes, blobUUID, file }) => {
+    const uploadData = request.uploadData;
+    if (uploadData && uploadData.length > 0) {
+      new MultiBufferReadable(
+        uploadData.map(({ bytes, blobUUID, file }) => {
           if (file) {
             return createReadStream(file);
           } else if (blobUUID) {
@@ -73,27 +70,10 @@ app.whenReady().then(async () => {
             return new BufferReadable(bytes);
           }
         })
-      );
+      ).pipe(req);
+    } else {
+      req.end();
     }
-
-    const data = await _request(
-      {
-        hostname: "127.0.0.1",
-        port: 8080,
-        path: request.url.substring("tarrow://.".length),
-        method: request.method,
-        headers: request.headers,
-      },
-      stream
-    );
-
-    callback({
-      data,
-      headers: data.headers,
-      method: request.method,
-      referrer: request.referrer,
-      statusCode: data.statusCode,
-    });
   });
 
   createWindow();
